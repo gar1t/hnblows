8aa03b4ab51a803cd55ab07096c764ee382b4474
garrett <garrett@cloudbees.com>
2013-03-11 18:43:50 +0000

    Multiple servers

--- src/hnb_app.erl
+++ src/hnb_app.erl
@@ -8,29 +8,38 @@ init() ->
     {ok, child_specs()}.
 
 child_specs() ->
-    server_mode_child_specs() ++ client_mode_child_specs().
+    server_specs() ++ client_mode_specs().
 
-server_mode_child_specs() ->
-    case server_mode() of
-        true ->
+server_specs() ->
+    case servers_env() of
+	[] -> [];
+	Servers ->
             [users_spec(),
              client_cleanup_sup_spec(),
-             client_handler_sup_spec(),
-             server_spec()];
-        false ->
-            []
+             client_handler_sup_spec()
+	     |server_specs(Servers)]
     end.
 
-client_mode_child_specs() ->
+servers_env() ->
+    case application:get_env(servers) of
+	{ok, Servers} -> Servers;
+	undefined -> []
+    end.
+
+server_specs(Servers) ->
+    [server_spec(Port) || Port <- Servers].
+
+server_spec(Port) ->
+    MFA = {hnb_server, start_link, [Port]},
+    Opts = [{id, server_child_id(Port)}],
+    {MFA, Opts}.
+
+server_child_id(Port) -> {server, Port}.
+
+client_mode_specs() ->
     case client_mode() of
         true -> [client_sup_spec()];
         false -> []
-    end.
-
-server_mode() ->
-    case application:get_env(server_mode) of
-        {ok, Val} -> Val;
-        undefined -> false
     end.
 
 client_mode() ->
@@ -46,9 +55,6 @@ client_cleanup_sup_spec() ->
 
 client_handler_sup_spec() ->
     {hnb_client_handler_sup, [supervisor]}.
-
-server_spec() ->
-    {hnb_server, start_link, []}.
 
 client_sup_spec() ->
     {hnb_client_sup, [supervisor]}.
